fa4d0d9ac4a5c31ccd6959d37424a38ffeee262e
Miguel Garcia <none>
2020-03-10 16:26:56 +0100

    after install added

---
+++ after-install/.config/after-install/install.sh
@@ -0,0 +1,8 @@
+sudo pacman -S xorg
+sudo pacman -S nvidia
+sudo pacman -S --noconfirm i3-gaps i3blocks i3status i3lock
+sudo pacman -S ranger
+sudo pacman -S thunar
+sudo pacman -S base-devel
+sudo pacman -S steam
+sudo pacman -S lutris

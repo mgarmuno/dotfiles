7ede93451a4bb77bb026304028eaf708b43b413a
Miguel Garcia <none>
2020-03-11 10:10:51 +0100

    after install

--- after-install/.config/after-install/install.sh
+++ after-install/.config/after-install/install.sh
@@ -1,8 +1,16 @@
 sudo pacman -S xorg
 sudo pacman -S nvidia
-sudo pacman -S --noconfirm i3-gaps i3blocks i3status i3lock
+sudo pacman -S i3-gaps i3blocks i3status i3lock
 sudo pacman -S ranger
 sudo pacman -S thunar
 sudo pacman -S base-devel
 sudo pacman -S steam
 sudo pacman -S lutris
+sudo pacman -S git
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+sudo makepkg -si
+cd
+yay -S popcorntime
+yay -S spotify
+yay -S 

156b0334eaa229ee9d10f643c44b50cc554638b5
mgarmuno <miguelgarmm@gmail.com>
2020-05-06 15:05:01 +0200

    fixes

--- screenlayout/.screenlayout/tri.sh
+++ screenlayout/.screenlayout/tri.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+xrandr --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off --output DVI-D-1-1 --off --output DP-1-3 --off --output DP-1-4 --off --output HDMI-1-4 --off --output DP-1-5 --off

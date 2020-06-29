0c66a44c3bcb162e4e808573d75391b7920776f2
mgarmuno <miguelgarmm@gamil.com>
2020-06-29 17:41:57 +0200

    add

--- i3/.config/i3/scripts/.lock.sh
+++ i3/.config/i3/scripts/.lock.sh
@@ -22,8 +22,8 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
 #BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
-#BLURTYPE="5x2" # 3.80s
-BLURTYPE="2x8" # 2.90s
+BLURTYPE="5x2" # 3.80s
+#BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
 
 # Get the screenshot, add the blur and lock the screen with it

908f8ae192bb31d0ab2775f436f890ec829329d3
mgarmuno <miguelgarmm@gmail.com>
2020-07-15 11:08:40 +0200

    Reddit is also blocked now

--- magneto-tools/.config/magneto-tools/anti-ads.sh
+++ magneto-tools/.config/magneto-tools/anti-ads.sh
@@ -34,21 +34,6 @@ echo >> /etc/hosts
 
 cat /tmp/hosts.txt >> /etc/hosts
 
-sed -i '/i.reddit.com/d' /etc/hosts > /etc/null
-sed -i '/nsfw.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/redd.it/d' /etc/hosts > /dev/null
-sed -i '/reddit.com/d' /etc/hosts > /dev/null
-sed -i '/www.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/amp-reddit-com.cdn.ampproject.org/d' /etc/hosts > /dev/null
-sed -i '/odd.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/new.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/reddit.map.fastly.net/d' /etc/hosts > /dev/null
-sed -i '/www.redditgifts.com/d' /etc/hosts > /dev/null
-sed -i '/www.redditmedia.com/d' /etc/hosts > /dev/null
-sed -i '/www.redditstatic.com/d' /etc/hosts > /dev/null
-sed -i '/emoji.redditmedia.com/d' /etc/hosts > /dev/null
-sed -i '/s.redditmedia.com/d' /etc/hosts > /dev/null
-
 rm /tmp/hosts.txt
 
 exit

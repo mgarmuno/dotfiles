f1a9b8118f9420594cca3084fbd76b6199faeff9
mgarmuno <miguelgarmm@gmail.com>
2021-01-17 18:46:58 +0100

    I3 changes

--- magneto-tools/.config/magneto-tools/anti-ads.sh
+++ magneto-tools/.config/magneto-tools/anti-ads.sh
@@ -28,26 +28,11 @@ fi
 
 echo >> /etc/hosts
 
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/social/hosts -O /tmp/hosts.txt
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts -O /tmp/hosts.txt
 
 echo >> /etc/hosts
 
 cat /tmp/hosts.txt >> /etc/hosts
-
-sed -i '/i.reddit.com/d' /etc/hosts > /etc/null
-sed -i '/nsfw.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/redd.it/d' /etc/hosts > /dev/null
-sed -i '/reddit.com/d' /etc/hosts > /dev/null
-sed -i '/www.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/amp-reddit-com.cdn.ampproject.org/d' /etc/hosts > /dev/null
-sed -i '/odd.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/new.reddit.com/d' /etc/hosts > /dev/null
-sed -i '/reddit.map.fastly.net/d' /etc/hosts > /dev/null
-sed -i '/www.redditgifts.com/d' /etc/hosts > /dev/null
-sed -i '/www.redditmedia.com/d' /etc/hosts > /dev/null
-sed -i '/www.redditstatic.com/d' /etc/hosts > /dev/null
-sed -i '/emoji.redditmedia.com/d' /etc/hosts > /dev/null
-sed -i '/s.redditmedia.com/d' /etc/hosts > /dev/null
 
 rm /tmp/hosts.txt
 

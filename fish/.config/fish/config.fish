55d8d30f57ae67321f460775a174b2b3304dfdff
mgarmuno <miguelgarmm@gmail.com>
2021-02-11 19:23:54 +0100

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,5 @@
 starship init fish | source
 
-# Start X at login
-if status --is-interactive
-	if test -z "$DISPLAY" -a $XDG_VTNR = 1
-		exec startx -- -keeptty
-	end
-end
-
 if status is-interactive
 	neofetch
 end

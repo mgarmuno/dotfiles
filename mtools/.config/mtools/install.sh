3047ce2777aab21c9105c2627f068a368d368d1d
mgarmuno <miguelgarmm@gmail.com>
2021-02-24 21:47:05 +0100

    magneto-tools to mtools

---
+++ mtools/.config/mtools/install.sh
@@ -0,0 +1,6 @@
+systemctl --user mask pulseaudio.socket
+
+curl -fsSL https://starship.rs/install.sh | bash
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

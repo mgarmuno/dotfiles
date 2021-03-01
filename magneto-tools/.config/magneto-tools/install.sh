d9d374d9044261f7a025d235d9ddb7ab06d1745c
mgarmuno <miguelgarmm@gmail.com>
2021-03-01 21:17:13 +0100

    deleted magneto-tools

--- magneto-tools/.config/magneto-tools/install.sh
+++
@@ -1,6 +0,0 @@
-systemctl --user mask pulseaudio.socket
-
-curl -fsSL https://starship.rs/install.sh | bash
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

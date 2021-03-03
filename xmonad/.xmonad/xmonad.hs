17f3494893757082c20ea37a476575f8ad116b87
Miguel Garcia del Moral <miguel.garciadelmoral@melia.com>
2021-03-03 16:57:27 +0100

    xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -5,13 +5,24 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myTerminal :: String
+myTerminal = "termite"
+
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+myKeys :: [(String, X())]
+myKeys =
+	[ ("M-<Return>", spawn (myTerminal))
+	]
+
 main = do
 	xmproc <- spawnPipe "xmobar"
-	xmonad $ docks defaultConfig { 
-		layoutHook = avoidStruts $ layoutHook defaultConfig,
-		logHook = dynamicLogWithPP xmobarPP {
-			ppOutput = hPutStrLn xmproc,
-			ppTitle = xmobarColor "green" "" . shorten 50
-		},
-		modMask = mod4Mask
+	xmonad $ docks defaultConfig
+		{ layoutHook = avoidStruts $ layoutHook defaultConfig
+		, logHook = dynamicLogWithPP xmobarPP
+			{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "green" "" . shorten 50
+		}
+		, modMask = myModMask
 	}

54fb9dc447b27c81adca8c2c92a6eb4fb664599e
mgarmuno <miguelgarmm@gmail.com>
2021-03-05 23:34:29 +0100

    dwm autostart scripts

---
+++ dwm/.dwm/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#blueman-applet
+setxkbmap -layout us -variant altgr-intl -option nodeadkeys
+feh --bg-scale /home/magneto/.config/walls/welcome_home_dracula.png
+
+exit 1
---
+++ dwm/.dwm/autostart_blocking.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh /home/magneto/.screenlayout/tri.sh
